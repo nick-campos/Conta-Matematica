95db12117ee85fd3c86dae5c4c4712d84d97e637
Nicolas Campos <nicolascampos611@gmail.com>
2025-03-26 13:02:38 -0300

    Criado as operações que serão feitas quando o usuário selecionar na página web

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 function calcular() {
 
-    var num1 = document.getElementById("numero1")
-    var num2 = document.getElementById("numero2")
-    var n1 = Number(num1.value)
-    var n2 = Number(num2.value)
-    var res = document.getElementById("res")
-    var soma = n1 + n2
+    var calculo = {
+    num1: document.getElementById("numero1"),
+    num2: document.getElementById("numero2"),
+    res: document.getElementById("res"),
+    opcoes: document.getElementById("operadores")
+    }
 
-    res.innerHTML = `A soma entre os dois número é ${soma}`
+    var resultado = {
+    n1: Number(calculo.num1.value),
+    n2: Number(calculo.num2.value),
+    }
+
+    var opcoes;
+    switch (calculo.opcoes.value) {
+    
+        case "+": 
+            opcoes = resultado.n1+resultado.n2;
+            break;
+    
+        case "-":
+            opcoes = resultado.n1-resultado.n2;
+            break;
+        
+        case "/":
+            opcoes = parseFloat((resultado.n1/resultado.n2).toFixed(2));
+            break;
+
+        case "*":
+            opcoes = resultado.n1*resultado.n2;
+            break;
+    }
+
+    calculo.res.innerHTML = `O cálculo entre ${resultado.n1} e ${resultado.n2} é ${opcoes}`
 }
